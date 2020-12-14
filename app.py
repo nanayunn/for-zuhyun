ccd541b47d4f75d2466a29639e4fe70ebd21ec2e
nanayunn <58680504+nanayunn@users.noreply.github.com>
2020-12-14 13:45:22 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,16 +5,15 @@ app = Flask(__name__)
 
 # list of cat images
 images = [
-        "https://opgg-com-image.akamaized.net/attach/images/20200325054401.1038024.gif",
-        "https://img2.quasarzone.co.kr/web/editor/1912/1912obj___873000268.gif",
-        "http://4.bp.blogspot.com/-GuzHHRgw6wg/UgvrdcfsfEI/AAAAAAAAACw/YBbtXcZe4c8/s320/cat+banging.gif",
-	"http://kids.dongascience.com/upload/posting/2020/08/75ccac6cabeba28942e9b7eeef67106c.gif",
-	"http://kids.dongascience.com/upload/posting/2020/08/20a96892c54e5d7f956a50a78d3e634d.gif",
-	"http://kids.dongascience.com/upload/posting/2020/08/26c9ff6cee4f797bc170271df98c9810.gif",
-	"http://kids.dongascience.com/upload/posting/2020/08/e19e355fb16792d329bb8ab7b9f33723.gif",
-	"https://upload2.inven.co.kr/upload/2018/05/26/bbs/i14701944056.gif",
-	"https://upload2.inven.co.kr/upload/2018/05/26/bbs/i14771846318.gif"
-        ]
+        "https://files.slack.com/files-pri/T024TCSN9-F01GM16VA5U/ios_______.gif",
+	"https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2Fu1MBP%2FbtqEgQZP2NJ%2FucXogL5koT9K7idkoZioJk%2Fimg.gif",
+	"https://mblogthumb-phinf.pstatic.net/MjAxODAzMjZfMTIy/MDAxNTIxOTkwODA4NDQ5.hutL1xCVQCBV7W89PGCXY8ExkP3fgDwK9mMPWzcitgMg.ju1wEuU9OeYwTrMK2x3Fxn6HdNOqSWLg4U2v1N7n2dwg.GIF.gpal762/m_entertainer1-20180325-163249-000.gif?type=w800",
+	"https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2F8zTkq%2FbtqHVosQ1vk%2FlekwS4c6aqgUGcoSsvZOx0%2Fimg.gif",
+	"https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FvSjnD%2FbtqH0qDuArx%2FLcdVj5Qvrlb3ygqKo0lFyk%2Fimg.gif",
+	"https://mblogthumb-phinf.pstatic.net/MjAxODAyMjdfMTk1/MDAxNTE5NzMxNjE5MTI4.hcBxr6amiZnEq5AVaWNqFGTyLib3OoS9gej4VNXzBhUg.gm8YHw0f1pJlaRGTdXnkH3w1dFQeVWnqE7cwhQzrsccg.GIF.thatluv/1519606689664.gif?type=w2",
+	"https://mblogthumb-phinf.pstatic.net/MjAxODAyMjdfMjUw/MDAxNTE5NzMxNjIxNjM1.xrdjUcmCW1hkUM53J-5kYB-1i8808Ko-SwgvGHRP020g.QeDkwQ6cVGYSdGkGoriszfxn_B5VCxc5RNMowmCswCIg.GIF.thatluv/1519606683127.gif?type=w2"
+	
+]
 
 @app.route('/')
 def index():
